6e6b759d117794644f9f49c07932c2ad77d4f909
Gonzalo Ponce de León <gonzalopj125@gmail.com>
2025-10-08 18:49:25 -0500

    avance

--- scripts/main.py
+++ scripts/main.py
@@ -7,6 +7,7 @@ from PIL import Image
 
 import numpy as np
 from math import radians
+from windrose import WindroseAxes
 
 
 from extract import extract
@@ -18,7 +19,8 @@ if __name__ == "__main__":
     # Configuración personalizable
     for con in CONFIG:
         periodo_pico = "tp_s"
-        intervalos = con['rangos_valor']
+        hs_bins = [0, 0.35, 0.4, 0.45, 0.6]
+        tp_bins = [0, 10, 13, 16, 20]
 
         #1. Extraer la data
         df = extract(con['path'])
@@ -33,11 +35,21 @@ if __name__ == "__main__":
 
         ## Altura significativa
         #get_table_frequency(axes[2], df, par1="dir_rad", par2="hs")
+        get_polar_rose_plot(axes[0], df, r = 'hs_m', theta = "dirtp_rad", intervals = hs_bins)
+        get_histogram(axes[1], df['hs_m'], bins=hs_bins, xlabel='hs_m', title='Histograma de '+'hs_m')
 
         ## Periodo pico
-        get_polar_rose_plot(axes[0], df, r = periodo_pico, theta = "dirtp_rad", intervals = intervalos)
-        get_histogram(axes[3], df[periodo_pico], bins=intervalos, xlabel=periodo_pico, title='Histograma de '+periodo_pico)
-        get_polar_rose_plot(axes[1], df, r = 'hs_m', theta = "dirtp_rad", intervals = intervalos)
+        get_polar_rose_plot(axes[2], df, r = 'tp_s', theta = "dirtp_rad", intervals = tp_bins)
+        get_histogram(axes[3], df['tp_s'], bins=tp_bins, xlabel='tp_s', title='Histograma de '+'tp_s')
+
+
+        ax = WindroseAxes.from_ax()
+        ax.set(theta_zero_location="N")
+        ax.set(theta_direction="clockwise")
+        ax.bar(df["dirtp_dgs"], df["tp_s"], normed=True, bins = tp_bins, opening=0.8, nsector=16, edgecolor='white') #By default, the offset is zero, and the first sector is [-360/nsector/2, 360/nsector/2] or [-11.25, 11.25] for nsector=16.
+        ax.set_title("tp_s", fontsize=12, weight='bold')
+        ax.set_legend()
+
         #get_table_frequency(axes[2], df, par1="dir_rad", par2="hs")
 
         plt.tight_layout()
--- scripts/visualizations.py
+++ scripts/visualizations.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from windrose import WindroseAxes
 
 from constants import DIR16_LABELS
 
